b9fa0152e34c68e7da595e8edf8af3940cd2eed9
Tim <timvzalingen@gmail.com>
2016-02-10 12:17:01 +0100

    orthogonal_projection 3.2 done. Simply put the matrix Morth from the slides in and added 'make run' to Makefile. Cheers!

--- wk2/orthogonal_projection/ortho.c
+++ wk2/orthogonal_projection/ortho.c
@@ -5,11 +5,10 @@
  * Date ............ 01.09.2006
  * Created by ...... Jurgen Sturm 
  *
- * Student name ....
- * Student email ... 
- * Collegekaart ....
- * Date ............
- * Comments ........
+ * Student name .... Tim van Zalingen & Maico Timmerman
+ * Student email ... timvzalingen@gmail.com & maico.timmerman@gmail.com
+ * Collegekaart .... 10784012 & 10542590
+ * Date ............ 12 Februari 2016
  *
  *
  * (always fill in these fields before submitting!!)
@@ -32,5 +31,14 @@ void myOrtho(GLdouble left,
              GLdouble top,
              GLdouble near,
              GLdouble far) {
-
+    GLdouble r_min_l = right - left;
+    GLdouble t_min_b = top - bottom;
+    GLdouble n_min_f = near - far;
+    GLdouble Morth[16] = {
+        2.0/(r_min_l), 0.0,           0.0,           (right+left)/(right-left),
+        0.0,           2.0/(t_min_b), 0.0,           (top+bottom)/(top-bottom),
+        0.0,           0.0,           2.0/(n_min_f), (near+far)/(near-far),
+        0.0,           0.0,           0.0,           1.0
+    };
+    glMultMatrixd(Morth);
 }
