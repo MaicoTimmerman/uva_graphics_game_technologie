6f888b7df42f76835f13c0e37d92c7464f2f85d0
Maico Timmerman <maico.timmerman@gmail.com>
2016-02-29 23:36:25 +0100

    Blinn-phong shading finished

--- wk5/framework/shaders.c
+++ wk5/framework/shaders.c
@@ -27,10 +27,6 @@
 // based on normal, light position, etc. As such, it merely creates
 // a "silhouette" of an object.
 
-float max(float a, float b) {
-    return a > b ? a : b;
-}
-
 vec3
 shade_constant(intersection_point ip)
 {
@@ -45,13 +41,14 @@ shade_matte(intersection_point ip)
     for (int i = 0; i < scene_num_lights; ++i) {
         vec3 light_pos = scene_lights[i].position;
 
+        // Calculate the vector from the point to the light source.
         shade_vec = v3_normalize(v3_subtract(scene_lights[i].position, ip.p));
         if (!shadow_check(v3_multiply(ip.p, 1.001), shade_vec)) {
-            light += max(0, scene_lights[i].intensity * v3_dotprod(ip.n, shade_vec));
+            light += fmax(0, scene_lights[i].intensity * v3_dotprod(ip.n, shade_vec));
         }
     }
 
-    if (light > 1.0) light = 1.0;
+    /* if (light > 1.0) light = 1.0; */
 
     return v3_create(light, light, light);
 }
@@ -59,6 +56,44 @@ shade_matte(intersection_point ip)
 vec3
 shade_blinn_phong(intersection_point ip)
 {
+    float light_diffuse = scene_ambient_light;
+    float light_specular = 0;
+
+    float cur_diffuse = 0;
+    float cur_specular = 0;
+
+    vec3 cd = v3_create(1.0, 0.0, 0.0);
+    vec3 cs = v3_create(1.0, 1.0, 1.0);
+    vec3 shade_vec;
+
+    // For all the light sources calculate the diffuse and specular components.
+    for (int i = 0; i < scene_num_lights; ++i) {
+
+        // Calculate the vector from the point to the light source.
+        vec3 li = v3_normalize(v3_subtract(scene_lights[i].position, ip.p));
+
+        // If there is no shadow interception by an object, calculated the
+        // cummulative light strength
+        if (!shadow_check(v3_multiply(ip.p, 1.001), li)) {
+            cur_diffuse += scene_lights[i].intensity * fmax(0, v3_dotprod(ip.n, li));
+        }
+
+        // calulate the halfway vector
+        vec3 h = v3_normalize(v3_add(ip.n, li));
+        if (!shadow_check(v3_multiply(ip.p, 1.001), h)) {
+            cur_specular += scene_lights[i].intensity * pow(fmax(0, v3_dotprod(ip.n, li)), 50);
+        }
+    }
+
+    light_diffuse += (0.8 * cur_diffuse);
+    light_specular += (0.5 * cur_specular);
+
+    return v3_create(light_diffuse+light_specular,
+                     light_diffuse+light_specular,
+                     light_diffuse+light_specular);
+
+
+    /* float */
     return v3_create(1, 0, 0);
 }
 
