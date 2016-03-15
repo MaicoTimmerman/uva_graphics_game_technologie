71d4c89401d18fe1861732e959d06c75058990cd
Tim <Tim>
2016-03-15 13:32:53 +0100

    Framework en beetje beweging met 'a' en 'd' voor graphics week 7.

---
+++ wk7/2D_assignment_framework/Assets/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	private float xSpeed;
+	private float ySpeed;
+
+	// Use this for initialization
+	void Start () {
+		xSpeed = Input.GetAxis("HorizontalA");
+		ySpeed = Input.GetAxis("Vertical");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+		float speed = 0f;
+		if (Input.GetButtonDown ("HorizontalA")) {
+			speed -= -5f;
+		}
+		if (Input.GetButtonDown ("HorizontalD")) {
+			speed += 5f;
+		}
+		if (Input.GetButtonUp ("HorizontalD") && Input.GetButtonUp ("HorizontalA")) {
+			speed = 0f;
+		}
+		xSpeed += speed;
+		if (xSpeed > 5f) {
+			xSpeed = 5f;
+		} else if (xSpeed < 5f) {
+			xSpeed = -5f;
+		}
+			
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (xSpeed, 0f * ySpeed);
+	}
+}
